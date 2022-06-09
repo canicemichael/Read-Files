9fec7a9ef8288324f4c6acfbb62d4a1552c0d642
canicemichael <canicemichael@gmail.com>
2022-06-09 23:05:29 +0100

    Update

---
+++ fsReadfile.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+// fs.readFile('./sample2.json', 'utf-8', (err, jsonString) => {
+//     if(err){
+//         console.log(err);
+//     } else{
+//         try {
+//             const data = JSON.parse(jsonString);
+//             console.log(data);
+//         } catch (error) {
+//             console.log('Error in the JSON', error);
+//         }     
+        
+//     }    
+// })
+
+function jsonReader(filePath, cb) {
+    fs.readFile(filePath, 'utf-8', (err, fileData) => {
+        if (err) {
+            return cb && cb(err);
+        }
+
+        try {
+            const object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        } catch (error) {
+            return cb && cb(err);
+        }
+    })
+}
+
+function done(path){
+    jsonReader(path, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(data);
+        }
+    })
+}
+
+done('./sample2.json');
